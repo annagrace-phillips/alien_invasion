a9b6f8a32f429c568f0e872aa040d869ab9c8d91
Anna Phillips <annagrace_phillips1@baylor.edu>
2021-03-14 15:05:41 -0500

    creating_alieninstance

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -20,6 +21,9 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
         #set the background color
         self.bg_color = (230, 230, 230)
@@ -85,6 +89,13 @@ class AlienInvasion:
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
+
+    def _create_fleet(self):
+        """Create the fleet of aliens."""
+        #make an alien
+        alien = Alien(self)
+        self.aliens.add(alien)
         
 
         #make the most recently drawn screen visible
