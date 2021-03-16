591398e25a93e79f237dcb2adeba61477737df39
Anna Phillips <annagrace_phillips1@baylor.edu>
2021-03-16 00:03:34 -0500

    dropfleet_and_changedirection

--- alien_invasion.py
+++ alien_invasion.py
@@ -50,7 +50,11 @@ class AlienInvasion:
             self._update_screen()
 
     def _update_aliens(self):
-        """Update the positions of all aliens in the fleet."""
+        """
+        Check if the fleet is at an edge, then
+        update the positions of all aliens in the fleet.
+        """
+        self._check_fleet_edges()
         self.aliens.update()
             
     def _check_events(self):
@@ -116,7 +120,19 @@ class AlienInvasion:
         alien.rect.y = alien_height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
         
-    
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge"""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction"""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
     def _update_screen(self):
         """Update images on the screen & flip to the new screen."""  
         #redraw the screen during each pass through the loop
