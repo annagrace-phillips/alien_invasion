7346ea5a3de2e265b97f1b1d97986dbda5412d7a
Anna Phillips <annagrace_phillips1@baylor.edu>
2021-03-16 08:42:31 -0500

    score_all_hits

--- alien_invasion.py
+++ alien_invasion.py
@@ -72,7 +72,8 @@ class AlienInvasion:
                 self.bullets, self.aliens, True, True)
 
         if collisions:
-            self.stats.score += self.settings.alien_points
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
 
         if not self.aliens:
@@ -140,6 +141,7 @@ class AlienInvasion:
             #reset the game statistics
             self.stats.reset_stats()
             self.stats.game_active = True
+            self.sb.prep_score()
 
             #get rid of any remaining aliens and bullets
             self.aliens.empty()
