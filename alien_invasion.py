aa00f00d5c2990765d5dd71243f04e8d43e92a95
Anna Phillips <annagrace_phillips1@baylor.edu>
2021-03-15 23:46:29 -0500

    moving_aliens_right

--- alien_invasion.py
+++ alien_invasion.py
@@ -34,6 +34,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
     def _update_bullets(self):
@@ -47,6 +48,10 @@ class AlienInvasion:
                 self.bullets.remove(bullet)
 
             self._update_screen()
+
+    def _update_aliens(self):
+        """Update the positions of all aliens in the fleet."""
+        self.aliens.update()
             
     def _check_events(self):
         """Respond to keypresses and mouse events."""
